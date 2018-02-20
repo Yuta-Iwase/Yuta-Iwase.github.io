d29a662f8f5761e251b55878a3de13b2493dae40
Owner <yutabk201@gmail.com>
2018-02-20 17:38:33 +0900

    edit up_btn and add copyright

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// スクロールの設定
+// 参考: http://klutche.org/archives/536/
 $(function(){
 	$('a[href^=#]').click(function(){
 		var speed = 500;
@@ -8,3 +10,9 @@ $(function(){
 		return false;
 	});
 });
+
+// 年が変わったときに自動的に年を更新させる
+var currentYear = new Date().getFullYear()
+if(currentYear > 2018){
+	document.getElementById("footer2_toYear").innerHTML= "-" + currentYear;
+}
